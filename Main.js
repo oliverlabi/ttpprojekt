7ee29ee14b61936d9a1c4d7d2ac9461b7431dc87
Oliver Labi <70663342+oliverlabi@users.noreply.github.com>
2021-06-07 15:35:18 +0300

    Klasside eraldamine

---
+++ Main.js
@@ -0,0 +1,98 @@
+class CurriculumCalculator{
+    constructor(){
+        this.currentSabbaticalLeave = 0;
+        this.currentAbroadStudies = 0;
+        this.curriculumChoice = $("#curriculum_dropdown :selected").text();
+        this.bachelorsCurriculums = ["Informaatika", "Infoteadus", "Matemaatika, majandusmatemaatika ja andmeanalüüs"];
+        this.mastersCurriculums = ["Haridustehnoloogia", "Infotehnoloogia juhtimine", "Infoteadus", "Informaatikaõpetaja", "Matemaatikaõpetaja", "Avatud ühiskonna tehnoloogiad", "Digitaalsed õpimängud", "Inimese ja arvuti interaktsioon", "Interaktsioonidisain"];
+        this.fullStudyLoadLowerLimit = 0;
+        this.studyLowerLimit = 0;
+        this.scenarioText = "";
+        this.studyLoad = "";
+        this.attendanceCount = $("#curriculum_attendance").val();
+        this.sabbaticalCount = $("#sabbatical_leave").val();
+        this.ectsCount = $("#ects_count").val();
+        this.abroadSemesterCount = $("#abroad_semester_count").val();
+        this.abroadEctsCount = $("#abroad_ects_count").val();
+        this.degree = "none";
+        this.universityAttendance = this.attendanceCount - this.sabbaticalCount;
+        this.init();
+    }
+
+    init(){
+        if(Validation.prototype.inputValidation() == 1){
+            $("#error").html("");
+            $("#result_error").html("");
+            $("#input_area_buttons").css("display", "none");
+            $("#result_area_buttons").css("display", "block");
+            $("#error").css("display", "none");
+            $("#result_error").css("display", "block");
+            Calculation.prototype.calcStudyLimits.call(this);
+            Calculation.prototype.calcStudyLoad.call(this);
+            Calculation.prototype.checkCurriculumDegree.call(this);
+            this.drawResultBox();
+        } else {
+            $("#error").html("Kontrollige üle sisestuslahtrid!");
+            $("#result_error").html("Kontrollige üle sisestuslahtrid!");
+        }
+        $("#new_calculation_button").on("click", ()=>{this.pageReload();});
+    }
+
+    
+    pageReload(){
+        location.reload();
+    }
+
+    drawResultBox(){
+        if($("#error").html("")){
+            $("#curriculum_result").html("Sinu õppekava: " + this.curriculumChoice);
+            $("#ects_result").html("Sinu ainepunktide arv: " + this.ectsCount + " EAP");
+            $("#result_padding").css("display", "block");
+            $("#results").css("display", "block");
+            Calculation.prototype.calcScenario.call(this);
+        }
+    }
+
+    
+}
+
+$("#abroad_yes").on("click", function(){
+    $("#abroad_input_area").css("display", "block");
+})
+
+$("#abroad_no").on("click", function(){
+    $("#abroad_input_area").css("display", "none");
+})
+
+$("#continue_button").on("click", function(){
+    if($("#curriculum_dropdown :selected").text() == "Vali õppekava..."){
+        $("#error").html("Vali õppekava!");
+    } else {
+        $("#error").html("");
+        $("#curriculum_choice_area").css("display", "none");
+        $("#curriculum_choice_area_buttons").css("display", "none");
+        $("#input_area").css("display", "block");
+        $("#input_area_buttons").css("display", "block");
+        $("#time_on_page").css("display", "block");
+    }
+    
+})
+
+$("#back_button").on("click", function(){
+    $("#curriculum_choice_area").css("display", "block");
+    $("#curriculum_choice_area_buttons").css("display", "block");
+    $("#input_area").css("display", "none");
+    $("#input_area_buttons").css("display", "none");
+    $("#time_on_page").css("display", "none");
+})
+
+
+$("#calculate_button").click(function(){
+    
+    let calculation = new CurriculumCalculator;
+})
+
+$("#result_calculate_button").click(function(){
+    let calculation = new CurriculumCalculator;
+})
+
